dd592f97a08ffb2a61cbfacc6e0909cc0249beb4
rayenharbi <rayenharbi123@gmail.com>
2023-08-17 12:38:12 +0100

    commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,64 @@
+import { useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { TbSearch } from "react-icons/tb";
+import { CgShoppingCart } from "react-icons/cg";
+import { AiOutlineHeart } from "react-icons/ai";
+import "./Header.scss";
+import Search from "./Search/Search";
+import { Context } from "../../utils/context";
+import Cart from "../Cart/Cart";
+import logo1 from "../../assets/logo1.png";
+
+const Header = () => {
+    const [scrolled, setScrolled] = useState(false);
+    const [searchModal, setSearchModal] = useState(false);
+    const navigate = useNavigate();
+    const handleScroll = () => {
+        const offset = window.scrollY;
+        if (offset > 200) {
+            setScrolled(true);
+        } else {
+            setScrolled(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+    }, []);
+
+    const { cartCount, showCart, setShowCart } = useContext(Context);
+
+    return (
+        <>
+            <header
+                className={`main-header ${scrolled ? "sticky-header" : ""}`}
+            >
+                <div className="header-content">
+                    <ul className="left">
+                        <li onClick={() => navigate("/")}>Home</li>
+                        <li onClick={() => navigate("/about")}>About</li>
+                        <li>Categories</li>
+                    </ul>
+                    <div className="center" onClick={() => navigate("/")}>
+                    <img src={logo1} style={{ width: '60px', height: 'auto'  }} alt=""/>
+                    </div>
+                    <div className="right">
+                        <TbSearch onClick={() => setSearchModal(true)} />
+                        <AiOutlineHeart />
+                        <span
+                            className="cart-icon"
+                            onClick={() => setShowCart(true)}
+                        >
+                            <CgShoppingCart />
+                            {!!cartCount && <span>{cartCount}</span>}
+                        </span>
+                    </div>
+                </div>
+            </header>
+            {searchModal && <Search setSearchModal={setSearchModal} />}
+            {showCart && <Cart />}
+        </>
+    );
+};
+
+export default Header;
---
+++ src/components/Home/Banner/Banner.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import "./Banner.scss";
+
+
+const Banner = () => {
+    return (
+        <div className="hero-banner" >
+            
+                
+            
+            <div className="content">
+                <div className="text-content">
+                    <h1>MADE BY TWO TEENAGERS</h1>
+                    <p>
+                        Convallis interdum purus adipiscing dis parturient
+                        posuere ac a quam a eleifend montes parturient posuere
+                        curae tempor
+                    </p>
+                    <div className="ctas">
+                        <div className="banner-cta">Read More</div>
+                        <div className="banner-cta v2">Shop Now</div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
